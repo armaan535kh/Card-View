1b870063ed69f20184c3af030fc95a26b466c947
armaan535kh <kh.armaan535@gmail.com>
2024-09-03 08:44:11 -0700

    Initial Commit

---
+++ Card View/Card_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_ViewApp.swift
+//  Card View
+//
+//  Created by Armaan Khan  on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Card_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
