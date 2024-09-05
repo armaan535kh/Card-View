3848de5a003897d901b5871d18972fd165f64546
armaan535kh <kh.armaan535@gmail.com>
2024-09-05 00:48:16 -0700

    creating a music card

--- Card View/ContentView.swift
+++ Card View/ContentView.swift
@@ -9,16 +9,79 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .foregroundStyle(.pink.gradient.opacity(0.8))
+                .ignoresSafeArea()
+            VStack {
+                GroupBox {
+                    GroupBox{
+                        MusicPlayerView()
+                    }
+                } label: {
+                    Label("Now playing",systemImage: "music.note")
+                }
+                .groupBoxStyle(.music)
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct MusicGroupBoxStyle: GroupBoxStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(alignment: .leading) {
+            configuration.label
+                .foregroundStyle(.pink)
+            
+            configuration.content
+        }
+        .padding()
+        .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 12))
+    }
+}
+
+extension GroupBoxStyle where Self == MusicGroupBoxStyle {
+    static var music: MusicGroupBoxStyle{.init()}
+}
+
+struct MusicPlayerView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                RoundedRectangle(cornerRadius: 8)
+                    .frame(width: 50, height: 50)
+                    .foregroundStyle(.secondary)
+                
+                VStack {
+                    Text("Cool Song Title")
+                        .font(.headline.bold())
+                    
+                    Text("Artist Name")
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                }
+                
+                Spacer()
+            }
+            .padding(.bottom, 8)
+            
+            ProgressView( value: 0.4, total: 1)
+                .tint(.secondary)
+                .padding(.bottom, 20)
+            
+            HStack(spacing: 30) {
+                Image(systemName: "backward.fill")
+                Image(systemName: "pause.fill")
+                Image(systemName: "forward.fill")
+            }
+            .foregroundStyle(.secondary)
+            .font(.title)
+        }
+    }
 }
